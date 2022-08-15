a0306e0d465f3a01b7e548cafe369633102c668a
NMcVayZag <Nickwmcvay15@gmail.com>
2022-08-15 13:11:44 -0600

    First Func

--- main.py
+++ main.py
@@ -20,6 +20,30 @@ damages = ['Damages not recorded', '100M', 'Damages not recorded', '40M', '27.9M
 deaths = [90,4000,16,3103,179,184,408,682,5,1023,43,319,688,259,37,11,2068,269,318,107,65,19325,51,124,17,1836,125,87,45,133,603,138,3057,74]
 
 # write your update damages function here:
+def clean_damages(lista):
+    nlist = []
+    for i in lista:
+        if i == "Damages not recorded":
+            nlist.append(i)
+        elif "M" in list(i): 
+            data = list(i)
+            data.pop()
+            data = "".join(data)
+            data = round(float(data),2)
+            data = data *1000000
+            nlist.append(data)
+        else:
+            data = list(i)
+            data.pop()
+            data = "".join(data)
+            data = round(float(data),2)
+            data = data *1000000000
+            nlist.append(data)
+    return(nlist)
+damages_n = clean_damages(damages)
+damages = damages_n
+print(damages)
+
 
 
 
